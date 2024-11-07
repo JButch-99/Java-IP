37ebfe6c30e05742c2c89d2fcd341b678a589fac
Josh Butcher <sjwb1999@gmail.com>
2024-11-07 15:59:36 -0800

    update HTML, JS, and CSS files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", function() {
-  
+  const confirmButton = document.getElementById("confirm-button");
+
+  function confirmQuiz() {
+    let confirm = document.getElementById("confirm-box");
+    
+    if (confirm.checked) {
+      alert("Test");
+    }
+    
+  }
+
+  confirmButton.addEventListener("click", confirmQuiz);
 })
