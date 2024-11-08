7c77960fa35b9625144ac723843699d44d572a67
Josh Butcher <sjwb1999@gmail.com>
2024-11-08 14:09:06 -0800

    make updates in regards to body-3, JS, and CSS properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 window.addEventListener("load", function() {
   const confirmButton = document.getElementById("confirm-button");
 
-  function confirmQuiz() {
+  function confirmQuiz(e) {
+    e.preventDefault();
     let confirm = document.getElementById("confirm-box");
+    const cover = document.getElementById("cover");
+    const quizBody = document.getElementById("quiz-body");
     
     if (confirm.checked) {
-      alert("Test");
+      cover.setAttribute("class", "hidden");
+      quizBody.removeAttribute("class", "hidden");
+      quizBody.setAttribute("class", "quiz-box")
     }
     
   }
