7cb1b512f00955e067d429b06f04626c1d012e6f
Josh Butcher <sjwb1999@gmail.com>
2024-11-08 16:00:04 -0800

    update CSS and JS bodies.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
   const confirmButton = document.getElementById("confirm-button");
-
+  const question1Button = document.getElementById("question-one-button");
+  
   function confirmQuiz(e) {
     e.preventDefault();
     let confirm = document.getElementById("confirm-box");
@@ -12,8 +13,12 @@ window.addEventListener("load", function() {
       quizBody.removeAttribute("class", "hidden");
       quizBody.setAttribute("class", "quiz-box")
     }
+  }
+
+  function questionOne() {
     
   }
 
   confirmButton.addEventListener("click", confirmQuiz);
+  question1Button.addEventListener("click", questionOne);
 })
